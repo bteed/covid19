2c27ed6226ef77f43b0c03e4591a2be4a4119575
Benjamin Teed <benjamin.teed@gmail.com>
2020-03-25 17:31:55 -0600

    reflect change to JHU data structure

--- R/cvd_data.R
+++ R/cvd_data.R
@@ -9,17 +9,15 @@
 cvd_data <- function(){
 
   suppressMessages({
-    confirmed <- readr::read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv")
-    recovered <- readr::read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Recovered.csv")
-    deaths <- readr::read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.csv")
+    confirmed <- readr::read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv")
+    deaths <- readr::read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv")
   })
 
   long_confirmed <- tidyr::pivot_longer(confirmed, 5:ncol(confirmed), names_to = "date", values_to = "confirmed")
-  long_recovered <- tidyr::pivot_longer(recovered, 5:ncol(recovered), names_to = "date", values_to = "recovered")
   long_deaths <- tidyr::pivot_longer(deaths, 5:ncol(deaths), names_to = "date", values_to = "deaths")
 
   invisible(capture.output({
-    df <- dplyr::left_join(dplyr::left_join(long_confirmed, long_recovered), long_deaths)
+    df <- dplyr::left_join(long_confirmed, long_deaths)
   }))
 
   df$date <- as.Date.character(df$date, tryFormats = "%m/%d/%y")
